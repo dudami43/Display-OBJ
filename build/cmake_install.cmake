36ddc0570a4c36e778f73ab0aeb09762654acb20
dudami43 <dudami43@gmail.com>
2018-11-02 17:33:46 -0300

    Parser

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/jogo")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/flight-simulator")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
--- main.cpp
+++ main.cpp
@@ -1,21 +1,4 @@
-//
-// INF390 - Computação Gráfica
-//
-// Visualizaçao 3D
-// Desenha um cubo centrado na origem com faces de -1 a 1
-// coloridas com diferentes cores
-//
-// Permite que a posicao do observador seja alterada usando o teclado
-// X -> soma uma unidade na coordenada x do observador
-// x -> subtrai  uma unidade na coordenada x do observador
-// Y -> soma uma unidade na coordenada y do observador
-// y -> subtrai  uma unidade na coordenada y do observador
-// Z -> soma uma unidade na coordenada z do observador
-// z -> subtrai  uma unidade na coordenada z do observador
-//
-// Marcus V. A. Andrade - 25/09/2008
-//
-
+#include <Model/OBJ.h>
 #include <GL/gl.h>
 #include <GL/glut.h>
 
@@ -25,76 +8,24 @@ void display(void);
 void keyboard(unsigned char key, int x, int y);
 void mouse(int button, int state, int x, int y);
 
-//funcao para desenhar um cubo colorido (desenha as suas 6 faces)
-void colorcube();
 
-//tamanho da janela em pixels
-int largurajanela = 400, alturajanela = 300;
-
-//vertices do cubo
-/* ------------------------------------------
-              vertices do cubo
-
-      5 ----- 6
-     /|      /|
-    1 ----- 2 |
-    | 4 ....| 7
-    |/      |/
-    0 ----- 3
-
-*/
-
-GLfloat vertices[8][3] = { {-1.0,-1.0,1.0},{-1.0,1.0,1.0},{1.0,1.0,1.0},{1.0,-1.0,1.0},
-{-1.0,-1.0,-1.0},{-1.0,1.0,-1.0},{1.0,1.0,-1.0},{1.0,-1.0,-1.0} };
-
-//cor de cada face do cubo
-GLfloat colors[6][3] = { {0.0,0.0,0.0} , {1.0,0.0,0.0}, {1.0,1.0,0.0}, {0.0,1.0,0.0},
-{0.0,0.0,1.0} , {1.0,0.0,1.0}};
 
 //posicao do observador (camera)
 GLdouble viewer[] = {2.0, 2.0, 3.0};
+Model model_teste;
 
-// desenha uma face do cubo
-// a, b, c e d sao indices no vetor de vertices
-// ncolor eh um indice no vetor de cores
-void quad(int a, int b, int c, int d, int ncolor) {
-    glColor3fv(colors[ncolor]);
-    glBegin(GL_QUADS);
-        glVertex3fv(vertices[a]);
-        glVertex3fv(vertices[b]);
-        glVertex3fv(vertices[c]);
-        glVertex3fv(vertices[d]);
-    glEnd();
-}
-
-//desenha o cubo (faces no sentido anti-horario - face externa)
-void colorcube() {
-
-    // Faces em sentido anti-horario
-    quad(0,3,2,1,0);
-    quad(2,3,7,6,1);
-    quad(0,4,7,3,2);
-    quad(1,2,6,5,3);
-    quad(4,5,6,7,4);
-    quad(0,1,5,4,5);
-}
-
-// define o tipo de projecao
-// glOrtho(left, right, bottom, top, near, far) -> projecao paralela
-// glFrustum(left, right, bottom, top, near, far) -> projecao perspectiva
-// gluPerspective(viewAngle, aspectRatio, near, far) -> projecao perspectiva
 void init(void) {
     glClearColor(1.0, 1.0, 1.0, 1.0); // cor para limpeza do buffer
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 
-    //glOrtho(-2.0, 2.0, -2.0, 2.0, 2.0, 20.0); // projeção paralela
+    //glOrtho(-2.0, 2.0, -2.0, 2.0, 2.0, 20.0); // projeï¿½ï¿½o paralela
 
-   // glFrustum(-2.0, 2.0, -2.0, 2.0, 2.0, 20.0); // projeção perspectiva
+   // glFrustum(-2.0, 2.0, -2.0, 2.0, 2.0, 20.0); // projeï¿½ï¿½o perspectiva
 
-   glFrustum(-2.0, 2.0, -2.0, 2.0, 2.0, 20.0); // projeção perspectiva
+   glFrustum(-2.0, 2.0, -2.0, 2.0, 2.0, 20.0); // projeï¿½ï¿½o perspectiva
 
-    //gluPerspective(70.0,1.0,2.0,20.0); // projeção perspectiva
+    //gluPerspective(70.0,1.0,2.0,20.0); // projeï¿½ï¿½o perspectiva
     glMatrixMode(GL_MODELVIEW);
 
    //  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //limpa a janela
@@ -106,8 +37,6 @@ void display(void) {
     gluLookAt(viewer[0],viewer[1],viewer[2], // define posicao do observador
     0.0, 0.0, 0.0,                           // ponto de interesse (foco)
     0.0, 1.0, 0.0);                          // vetor de "view up"
-
-    colorcube(); //desenha o cubo
 
     glFlush();
  //   glutSwapBuffers(); //usando double buffer (para evitar 'flicker')
@@ -124,21 +53,28 @@ void keyboard(unsigned char key, int x, int y) {
     display();
 }
 
+void carregarModelos()
+{
+    OBJ obj("teapot.obj");
+    obj.load(model_teste);
+}
+
 
 int main(int argc, char **argv) {
+    carregarModelos();
     glutInit(&argc,argv); //inicializa a glut
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH); //tipo de buffer/cores
-    glutInitWindowSize(largurajanela, alturajanela); //dimensões da janela
+    glutInitWindowSize(500, 500); //dimensï¿½es da janela
     glutInitWindowPosition(100, 100); //posicao da janela
-    glutCreateWindow("Visualizacao 3D - Movimento de camera "); //cria a janela
+    glutCreateWindow("Trabalho 3"); //cria a janela
     init();
-    glutDisplayFunc(display); //determina função callback de desenho
-    glutKeyboardFunc(keyboard); //determina função callback de teclado
+    glutDisplayFunc(display); //determina funï¿½ï¿½o callback de desenho
+    glutKeyboardFunc(keyboard); //determina funï¿½ï¿½o callback de teclado
 
-    glEnable(GL_DEPTH_TEST); //habilita remoção de superfícies ocultas usando Z-Buffer
+    glEnable(GL_DEPTH_TEST); //habilita remoï¿½ï¿½o de superfï¿½cies ocultas usando Z-Buffer
 
 
- //   glEnable (GL_CULL_FACE); //habilita remoção de superfícies ocultas usando Back Facxe Culling
+ //   glEnable (GL_CULL_FACE); //habilita remoï¿½ï¿½o de superfï¿½cies ocultas usando Back Facxe Culling
  //   glCullFace(GL_FRONT);    // Remove as faces da frente
  //   glCullFace(GL_BACK);    // Remove as faces de tras
 
