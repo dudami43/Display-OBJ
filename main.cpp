ebd0f0bac9d80878c0c36b7d773a61cbc62cdf1f
dudami43 <dudami43@gmail.com>
2018-11-03 17:33:22 -0300

    Parser .h

--- main.cpp
+++ main.cpp
@@ -1,84 +1,17 @@
-#include <Model/OBJ.h>
-#include <GL/gl.h>
-#include <GL/glut.h>
 #include <iostream>
 #include <stdlib.h>
+#include "parser.h"
+
 using namespace std;
 
-void init(void);
-void display(void);
-void keyboard(unsigned char key, int x, int y);
-void mouse(int button, int state, int x, int y);
-
-//posicao do observador (camera)
-GLdouble viewer[] = {2.0, 2.0, 3.0};
-Model model_teste;
-
-void init(void) {
-    glClearColor(1.0, 1.0, 1.0, 1.0); // cor para limpeza do buffer
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-
-    //glOrtho(-2.0, 2.0, -2.0, 2.0, 2.0, 20.0); // proje��o paralela
-
-   // glFrustum(-2.0, 2.0, -2.0, 2.0, 2.0, 20.0); // proje��o perspectiva
-
-   glFrustum(-2.0, 2.0, -2.0, 2.0, 2.0, 20.0); // proje��o perspectiva
-
-    //gluPerspective(70.0,1.0,2.0,20.0); // proje��o perspectiva
-    glMatrixMode(GL_MODELVIEW);
-
-   //  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //limpa a janela
-}
-
-void display(void) {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //limpa a janela
-    glLoadIdentity();
-    gluLookAt(viewer[0],viewer[1],viewer[2], // define posicao do observador
-    0.0, 0.0, 0.0,                           // ponto de interesse (foco)
-    0.0, 1.0, 0.0);                          // vetor de "view up"
-
-    glFlush();
- //   glutSwapBuffers(); //usando double buffer (para evitar 'flicker')
-}
-
-void keyboard(unsigned char key, int x, int y) {
-    if (key == 27) exit(0); //ESC
-    if (key == 'x') viewer[0] -= 1.0;
-    if (key == 'X') viewer[0] += 1.0;
-    if (key == 'y') viewer[1] -= 1.0;
-    if (key == 'Y') viewer[1] += 1.0;
-    if (key == 'z') viewer[2] -= 1.0;
-    if (key == 'Z') viewer[2] += 1.0;
-    display();
-}
-
-void carregarModelos()
+int main(int argc, char **argv)
 {
-    OBJ obj("teapot.obj");
-    obj.load(model_teste);
-    cout << model_teste.faces[0];
-}
-
-
-int main(int argc, char **argv) {
-    carregarModelos();
-    glutInit(&argc,argv); //inicializa a glut
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH); //tipo de buffer/cores
-    glutInitWindowSize(500, 500); //dimens�es da janela
-    glutInitWindowPosition(100, 100); //posicao da janela
-    glutCreateWindow("Trabalho 3"); //cria a janela
-    init();
-    glutDisplayFunc(display); //determina fun��o callback de desenho
-    glutKeyboardFunc(keyboard); //determina fun��o callback de teclado
-
-    glEnable(GL_DEPTH_TEST); //habilita remo��o de superf�cies ocultas usando Z-Buffer
-
-
- //   glEnable (GL_CULL_FACE); //habilita remo��o de superf�cies ocultas usando Back Facxe Culling
- //   glCullFace(GL_FRONT);    // Remove as faces da frente
- //   glCullFace(GL_BACK);    // Remove as faces de tras
-
-    glutMainLoop();
+    OBJ obj;
+    Modelo modelo;
+    modelo = obj.lerArquivo("Modelos/teapot.obj");
+    for (int i = 0; i < modelo.faces.size(); i++)
+    {
+        cout << modelo.faces[i].vertices[0].vertice << endl;
+    }
     return 0;
 }
---
+++ parser.h
@@ -0,0 +1,194 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <GL/gl.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Vertice
+{
+    GLfloat x = 0.0;
+    GLfloat y = 0.0;
+    GLfloat z = 0.0;
+    GLfloat w = 1.0;
+};
+
+struct Normal
+{
+    GLfloat x = 0.0;
+    GLfloat y = 0.0;
+    GLfloat z = 0.0;
+    GLfloat w = 1.0;
+};
+
+struct Textura
+{
+    GLfloat u = 0.0;
+    GLfloat v = 0.0;
+};
+
+struct Vertice_Face
+{
+    int vertice = 0;
+    int textura = 0;
+    int normal = 0;
+};
+
+struct Face
+{
+    vector<Vertice_Face> vertices;
+};
+
+struct Modelo
+{
+    string mtl;
+    vector<Vertice> vertices;
+    vector<Textura> textura;
+    vector<Normal> normal;
+    vector<Face> faces;
+};
+
+class OBJ
+{
+  private:
+    void insertFace(int insercao, int aux, Vertice_Face &vf)
+    {
+        switch (insercao)
+        {
+        case 0:
+            vf.vertice = aux;
+            break;
+        case 1:
+            vf.textura = aux;
+            break;
+        case 2:
+            vf.normal = aux;
+            break;
+        }
+    }
+
+  public:
+    Modelo lerArquivo(string nome)
+    {
+        Modelo modelo;
+        Vertice vertice;
+        Normal normal;
+        Textura textura;
+        Vertice_Face vf;
+        Face face;
+        ifstream arquivo;
+        arquivo.open(nome);
+        char item[100], c;
+
+        if (arquivo.is_open())
+        {
+            while (!arquivo.eof())
+            {
+                arquivo >> item;
+                if (!strcmp(item, "mtlib"))
+                {
+                    arquivo >> modelo.mtl;
+                }
+                else if (!strcmp(item, "v"))
+                {
+                    arquivo >> vertice.x;
+                    arquivo >> vertice.y;
+                    arquivo >> vertice.z;
+                    arquivo.get(c);
+                    if (c != '\n')
+                    {
+                        arquivo >> vertice.w;
+                    }
+                    modelo.vertices.push_back(vertice);
+                }
+                else if (!strcmp(item, "vn"))
+                {
+                    arquivo >> normal.x;
+                    arquivo >> normal.y;
+                    arquivo >> normal.z;
+                    arquivo.get(c);
+                    if (c != '\n')
+                    {
+                        arquivo >> normal.w;
+                    }
+                    modelo.normal.push_back(normal);
+                }
+                else if (!strcmp(item, "vt"))
+                {
+                    arquivo >> textura.u;
+                    arquivo >> textura.v;
+                    modelo.textura.push_back(textura);
+                }
+                else if (!strcmp(item, "f"))
+                {
+                    char aux[100];
+                    int i = 0, insercao = 0;
+                    bool verifica = false;
+                    arquivo.get(c); // ignorar espaço em branco
+                    arquivo.get(c);
+                    while (true)
+                    {
+                        if (isdigit(c))
+                        {
+                            aux[i++] = c;
+                            verifica = true;
+                        }
+                        else if (c == '/')
+                        {
+                            aux[i++] = '\0';
+                            arquivo.get(c);
+                            if (c == '/')
+                            {
+                                vf.vertice = atoi(aux);
+                                insercao += 2;
+                                i = 0;
+                            }
+                            else
+                            {
+                                insertFace(insercao, atoi(aux), vf);
+                                insercao++;
+                                aux[0] = c;
+                                i = 1;
+                            }
+                            verifica = true;
+                        }
+                        else if (c == ' ')
+                        {
+                            aux[i++] = '\0';
+                            insertFace(insercao, atoi(aux), vf);
+                            insercao = 0;
+                            i = 0;
+                            verifica = true;
+                            face.vertices.push_back(vf);
+                        }
+                        else if (c == '\n')
+                        {
+                            aux[i++] = '\0';
+                            if (verifica)
+                            {
+                                insertFace(insercao, atoi(aux), vf);
+                                face.vertices.push_back(vf);
+                                modelo.faces.push_back(face);
+                                face.vertices.clear();
+                            }
+                            break;
+                        }
+                        arquivo.get(c);
+                    }
+                }
+            }
+        }
+        else
+        {
+            cout << "Erro ao abrir o arquivo";
+        }
+        arquivo.close();
+        return modelo;
+    }
+};
+
+#endif
