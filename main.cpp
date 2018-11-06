3e77107a53bfb05c368be05d290047e451ff474e
dudami43 <dudami43@gmail.com>
2018-11-05 22:41:50 -0200

    "Interface do alab com um bule estranho"

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
-#include <stdlib.h>
-#include "parser.h"
 #include <GL/gl.h>
 #include <GL/glut.h>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include "parser.h"
+
 using namespace std;
 
 void init(void);
@@ -13,8 +15,21 @@ void mouse(int button, int state, int x, int y);
 float width = 1000;
 float height = 600;
 Modelo modelo;
+
 //posicao do observador (camera)
 GLdouble viewer[] = {2.0, 2.0, 3.0};
+
+char texto[100];
+char objName[100], objNameInput[100];
+char rotacaoTexto[4][10];
+char translacaoTexto[3][10];
+char escalaTexto[3][10];
+char triangulos[50];
+char ms[50];
+
+float rotacao[4] = {0.0};
+float translacao[3] = {0.0};
+float escala[3] = {1.0};
 
 void init(void)
 {
@@ -25,16 +40,235 @@ void init(void)
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(0, width, 0, height);
+
+    for (int i = 0; i < 4; i++)
+    {
+        sprintf(rotacaoTexto[i], "0.0");
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        sprintf(translacaoTexto[i], "0.0");
+        sprintf(escalaTexto[i], "1.0");
+    }
+
+    sprintf(objName, "teste.obj");
+    sprintf(triangulos, "Triangulos: 0");
+    sprintf(ms, "0.0 ms");
 }
 
 void desenhaMenu()
 {
-    glColor3f(1.0, 0.1, 0.34);
-    glBegin(GL_POLYGON); // inicia o desenho do padrao na origem
-    glVertex2f(2 * (width / 3) + 10, 50);
-    glVertex2f(3000, 50);
-    glVertex2f(3000, 100);
-    glVertex2f(2 * (width / 3) + 10, 100);
+
+    //ms
+    glColor3f(1.0, 1.0, 1.0);
+    glPushMatrix();
+    glRasterPos2f(900, 20);
+    for (char *i = ms; *i != 0; i++)
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, *i);
+    glPopMatrix();
+
+    //numero triangulos
+    glColor3f(1.0, 1.0, 1.0);
+    glPushMatrix();
+    glRasterPos2f(676, 20);
+    for (char *i = triangulos; *i != 0; i++)
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, *i);
+    glPopMatrix();
+
+    //escala
+    glColor3f(1.0, 1.0, 1.0);
+    sprintf(texto, "Escala:");
+    glPushMatrix();
+    glRasterPos2f(676, 452);
+    for (char *i = texto; *i != 0; i++)
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, *i);
+    glPopMatrix();
+
+    glColor3f(0.0, 0.0, 0.0);
+    for (int j = 0; j < 3; j++)
+    {
+        glPushMatrix();
+        glRasterPos2f(850 + (j * 50) + 2, 452);
+        for (char *i = escalaTexto[j]; *i != 0; i++)
+            glutBitmapCharacter(GLUT_BITMAP_8_BY_13, *i);
+        glPopMatrix();
+    }
+
+    // 3 campos de texto
+    glColor3f(1.0, 1.0, 1.0);
+    //campo 3 (escalaZ)
+    glBegin(GL_POLYGON);
+    glVertex2f(950, 450);
+    glVertex2f(990, 450);
+    glVertex2f(990, 470);
+    glVertex2f(950, 470);
+    glEnd();
+
+    //campo 2 (escalaY)
+    glBegin(GL_POLYGON);
+    glVertex2f(900, 450);
+    glVertex2f(940, 450);
+    glVertex2f(940, 470);
+    glVertex2f(900, 470);
+    glEnd();
+
+    //campo 1 (escalaX)
+    glBegin(GL_POLYGON);
+    glVertex2f(850, 450);
+    glVertex2f(890, 450);
+    glVertex2f(890, 470);
+    glVertex2f(850, 470);
+    glEnd();
+
+    //rotacao
+    glColor3f(1.0, 1.0, 1.0);
+    sprintf(texto, "Rotacao:");
+    glPushMatrix();
+    glRasterPos2f(676, 482);
+    for (char *i = texto; *i != 0; i++)
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, *i);
+    glPopMatrix();
+
+    glColor3f(0.0, 0.0, 0.0);
+    for (int j = 0; j < 4; j++)
+    {
+        glPushMatrix();
+        glRasterPos2f(800 + (j * 50) + 2, 482);
+        for (char *i = rotacaoTexto[j]; *i != 0; i++)
+            glutBitmapCharacter(GLUT_BITMAP_8_BY_13, *i);
+        glPopMatrix();
+    }
+
+    //4 campos de texto
+    glColor3f(1.0, 1.0, 1.0);
+    //campo 4 (rotacaoZ)
+    glBegin(GL_POLYGON);
+    glVertex2f(950, 480);
+    glVertex2f(990, 480);
+    glVertex2f(990, 500);
+    glVertex2f(950, 500);
+    glEnd();
+
+    //campo 3 (rotacaoY)
+    glBegin(GL_POLYGON);
+    glVertex2f(900, 480);
+    glVertex2f(940, 480);
+    glVertex2f(940, 500);
+    glVertex2f(900, 500);
+    glEnd();
+
+    //campo 2 (rotacaoX)
+    glBegin(GL_POLYGON);
+    glVertex2f(850, 480);
+    glVertex2f(890, 480);
+    glVertex2f(890, 500);
+    glVertex2f(850, 500);
+    glEnd();
+
+    //campo 1 (rotacaoAngulo)
+    glBegin(GL_POLYGON);
+    glVertex2f(840, 480);
+    glVertex2f(800, 480);
+    glVertex2f(800, 500);
+    glVertex2f(840, 500);
+    glEnd();
+
+    //translacao
+    glColor3f(1.0, 1.0, 1.0);
+    sprintf(texto, "Translacao:");
+    glPushMatrix();
+    glRasterPos2f(676, 512);
+    for (char *i = texto; *i != 0; i++)
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, *i);
+    glPopMatrix();
+
+    glColor3f(0.0, 0.0, 0.0);
+    for (int j = 0; j < 3; j++)
+    {
+        glPushMatrix();
+        glRasterPos2f(850 + (j * 50) + 2, 512);
+        for (char *i = translacaoTexto[j]; *i != 0; i++)
+            glutBitmapCharacter(GLUT_BITMAP_8_BY_13, *i);
+        glPopMatrix();
+    }
+
+    // 3 campos de texto
+    glColor3f(1.0, 1.0, 1.0);
+    //campo 3 (translacaoZ)
+    glBegin(GL_POLYGON);
+    glVertex2f(950, 510);
+    glVertex2f(990, 510);
+    glVertex2f(990, 530);
+    glVertex2f(950, 530);
+    glEnd();
+
+    //campo 2 (translacaoY)
+    glBegin(GL_POLYGON);
+    glVertex2f(900, 510);
+    glVertex2f(940, 510);
+    glVertex2f(940, 530);
+    glVertex2f(900, 530);
+    glEnd();
+
+    //campo 1 (translacaoX)
+    glBegin(GL_POLYGON);
+    glVertex2f(850, 510);
+    glVertex2f(890, 510);
+    glVertex2f(890, 530);
+    glVertex2f(850, 530);
+    glEnd();
+
+    //nome arquivo
+    glColor3f(0.0, 0.0, 0.0);
+    glPushMatrix();
+    glRasterPos2f(676, 542);
+    for (char *i = objName; *i != 0; i++)
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, *i);
+    glPopMatrix();
+
+    //botao hidden
+    glColor3f(0.84, 0.8, 0.86);
+    glBegin(GL_POLYGON);
+    glVertex2f(970, 540);
+    glVertex2f(990, 540);
+    glVertex2f(990, 560);
+    glVertex2f(970, 560);
+    glEnd();
+
+    ///botao abrir arquivo texto
+    glColor3f(0.0, 0.0, 0.0);
+    sprintf(texto, "Importar");
+    glPushMatrix();
+    glRasterPos2f(872, 574);
+    for (char *i = texto; *i != 0; i++)
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, *i);
+    glPopMatrix();
+
+    //botao abrir arquivo
+    glColor3f(0.84, 0.8, 0.86);
+    glBegin(GL_POLYGON);
+    glVertex2f(870, 570);
+    glVertex2f(990, 570);
+    glVertex2f(990, 590);
+    glVertex2f(870, 590);
+    glEnd();
+
+    //campo texto abrir arquivo
+    glColor3f(1.0, 1.0, 1.0);
+    glBegin(GL_POLYGON);
+    glVertex2f(676, 570);
+    glVertex2f(860, 570);
+    glVertex2f(860, 590);
+    glVertex2f(676, 590);
+    glEnd();
+
+    //FUNDO
+    glColor3f(0.47, 0.02, 0.78);
+    glBegin(GL_POLYGON);
+    glVertex2f(2 * (width / 3), 0);
+    glVertex2f(width, 0);
+    glVertex2f(width, height);
+    glVertex2f(2 * (width / 3), height);
     glEnd();
 }
 
@@ -75,16 +309,17 @@ void display(void)
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 
-    gluOrtho2D(0, 500, 0, height); //gluPerspective(45.0, 0, 0.01, 1000.0);
+    gluOrtho2D(-20, 20, -20, 20);
+    //glFrustum(-3.0, 3.0, -3.225, 3.225, 2.0, 20.0);
+    carregarModelos();
+
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
     //desenhaObjeto();
-
-    /*gluLookAt(viewer[0],viewer[1],viewer[2], // define posicao do observador
-    0.0, 0.0, 0.0,                           // ponto de interesse (foco)
-    0.0, 1.0, 0.0);
-*/
+    /*gluLookAt(viewer[0], viewer[1], viewer[2], // define posicao do observador
+              0.0, 0.0, 0.0,                   // ponto de interesse (foco)
+              0.0, 1.0, 0.0);*/
 
     glViewport(2 * (width / 3), 0, width / 3, height);
     glMatrixMode(GL_PROJECTION);
@@ -93,9 +328,7 @@ void display(void)
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    //desenhaMenu();
-
-    //carregarModelos();
+    desenhaMenu();
 
     glFlush();
     glutSwapBuffers();
